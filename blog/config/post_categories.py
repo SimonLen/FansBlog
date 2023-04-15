a5fa2b16b2d9c8f6300cdcee8c0cc7acd670638a
semyonsorokin <simonlen.gba@gmail.com>
2023-04-15 16:54:09 +0300

    Models added

---
+++ blog/config/post_categories.py
@@ -0,0 +1,12 @@
+CATEGORIES = [
+    ('Tanks', 'Танки'),
+    ('Healers', 'Хилы'),
+    ('DD', 'ДД'),
+    ('Traders', 'Торговцы'),
+    ('Guild Masters', 'Гилдмастеры'),
+    ('Quest Givers', 'Квестгиверы'),
+    ('Blacksmiths', 'Кузнецы'),
+    ('Leatherworkers', 'Кожевники'),
+    ('Alchemists', 'Зельевары'),
+    ('Spellcasters', 'Мастера заклинаний'),
+]
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
 
-# Create your models here.
+from .config.post_categories import CATEGORIES
+
+
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    username = models.CharField(max_length=16, verbose_name='username')
+
+
+class Post(models.Model):
+    author_id = models.ForeignKey(Author, on_delete=models.CASCADE)
+    title = models.CharField(max_length=64, verbose_name='Заголовок')
+    content = RichTextField()
+    created_at = models.DateTimeField(auto_created=True, verbose_name='Дата и время создания')
+    category = models.CharField(max_length=16, choices=CATEGORIES, verbose_name='Категория')
+    likes = models.ManyToManyField(Author, blank=True, related_name='liked_posts')
+    dislikes = models.ManyToManyField(Author, blank=True, related_name='disliked_posts')
+
+
+class Comment(models.Model):
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author_id = models.ForeignKey(Author, on_delete=models.CASCADE)
+    content = models.CharField(max_length=1024,verbose_name='Сообщение')
+    likes = models.ManyToManyField(Author, blank=True, related_name='liked_comments')
+    dislikes = models.ManyToManyField(Author, blank=True, related_name='disliked_comments')
